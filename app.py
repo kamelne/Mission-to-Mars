a545f244c99ba6873c0c6cea614cc18c24efdb2d
Nizar Kamel <nizarekamel@gmail.com>
2022-05-01 18:40:23 -0400

    d2 complete

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
-
 @app.route("/")
 def index():
    mars = mongo.db.mars.find_one()
--- scraping.py
+++ scraping.py
@@ -9,15 +9,17 @@ import datetime as dt
 def scrape_all():
     # Initiate headless driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=True)
+    browser = Browser('chrome', **executable_path, headless=False)
     news_title, news_paragraph = mars_news(browser)
+    
     # Run all scraping functions and store results in dictionary
     data = {
         "news_title": news_title,
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres" : hemisphere_info(browser)
     }
     # Stop webdriver and return data
     browser.quit()
@@ -80,8 +82,8 @@ def featured_image(browser):
 
 def mars_facts():
     try:
-            # use 'read_html' to scrape the facts table into a data frame
-            df = pd.read_html('https://galaxyfacts-mars.com')[0]
+        # use 'read_html' to scrape the facts table into a data frame
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]
     except BaseException:
         return None
 
@@ -91,7 +93,26 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
-    
+
+def hemisphere_info(browser):
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+    browser.is_element_present_by_css('h3', wait_time=1)
+    hemisphere_image_urls = []
+    hem_elem = browser.find_by_css("h3")
+    try:
+        for i in range(4):
+            hem_elem[i].click()
+            hem_soup = soup(browser.html, 'html.parser')
+            hem_url = hem_soup.ul.find('a')['href']
+            hem_name = browser.find_by_css("h2").text
+            hemisphere_image_urls.append({ 'img_url' : url + hem_url, "title": hem_name} )
+            browser.back()
+    except BaseException:
+        return None
+        
+    return hemisphere_image_urls
+
 if __name__ == "__main__":
     # If running as script, print scraped data
     print(scrape_all())
